bf15e5d78142292c2bd5e39051171950664347f8
Yulia <yuliarandomchenko@gmail.com>
2021-07-22 23:19:25 +0300

    Initial commit

---
+++ chat-app/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  chat-app
+//
+//  Created by Y u l i a on 22.07.2021.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chat-app/chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_appApp.swift
+//  chat-app
+//
+//  Created by Y u l i a on 22.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
