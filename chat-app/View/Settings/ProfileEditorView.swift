29d6494cdb9b78a816cf4d49eac9b18e02c77dba
Yulia <yuliarandomchenko@gmail.com>
2021-07-25 11:48:34 +0300

    Add profile editor view

--- chat-app/View/Settings/ProfileEditorView.swift
+++ chat-app/View/Settings/ProfileEditorView.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct ProfileEditorView: View {
     var body: some View {
-        Text("Profile editor")
+        ZStack {
+            Color(.systemGroupedBackground)
+                .ignoresSafeArea()
+        }
+        
+        VStack {
+            VStack {
+                HStack {
+                    VStack {
+                        Image(systemName: "person.fill")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 64, height: 64, alignment: .center)
+                            .padding(30)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .clipShape(Circle())
+                        
+                        Button(
+                            action: {
+                                // MARK: - add from Gallery
+                            }, label: {
+                                Text("Edit Photo")
+                            }
+                        )
+                    }
+                }
+            }
+            
+            VStack {
+                HStack {
+                    Text("Status:")
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                .padding()
+                HStack {
+                    Text("Available")
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(.gray)
+                    
+                }.padding([.horizontal])
+            }
+            .padding()
+            // TODO: - modify prototype
+        }
     }
 }
 
--- chat-app/ViewModel/SettingsCellViewModel.swift
+++ chat-app/ViewModel/SettingsCellViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by Y u l i a on 25.07.2021.
 //
 
-import Foundation
+import SwiftUI
+
+enum SettingsViewModel: Int, CaseIterable {
+    case account
+    case notifications
+    case savedMessages
+    
+    var title: String {
+        switch self {
+        case .account: return "Account"
+        case .notifications: return "Notifications"
+        case .savedMessages: return "Saved Messages"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .account: return "key.fill"
+        case .notifications: return "bell.badge.fill"
+        case .savedMessages: return "star.fill"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .account: return .blue
+        case .notifications: return .red
+        case .savedMessages: return .yellow
+        }
+    }
+    
+}
